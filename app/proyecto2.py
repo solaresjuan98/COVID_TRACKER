3835e4ccd482d800b88ae015856d5a5d3f240704
Juan Antonio Solares <solaresjuan43@gmail.com>
2021-12-27 14:14:38 -0600

    commit

---
+++ app/proyecto2.py
@@ -0,0 +1,22 @@
+
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import plotly
+import plotly.express as px
+import plotly.figure_factory as ff
+import seaborn as sns
+from PIL import Image
+
+import streamlit as st
+
+
+## Main 
+st.write("""
+    # PROYECTO 2
+    
+""")
+
+
+
