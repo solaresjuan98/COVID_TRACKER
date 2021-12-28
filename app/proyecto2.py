1213ccf1ba344cc4cd1eca47b69ec64ec4b96525
Juan Antonio Solares <solaresjuan43@gmail.com>
2021-12-28 13:28:04 -0600

    commit

--- app/proyecto2.py
+++ app/proyecto2.py
@@ -1,11 +1,119 @@
+import datetime as dt
+from os import write
 
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+import plotly
+import plotly.express as px
+import plotly.figure_factory as ff
+import seaborn as sns
 import streamlit as st
+from pandas._config.config import options
+from pandas.core.frame import DataFrame
 from PIL import Image
 
-from covidcases import covidInfectionTendence
-from coviddeaths import covidDeathsByCountry,covidDeathsPredictionByDep 
+# from covidcases import covidInfectionTendence
+# from coviddeaths import covidDeathsByCountry,covidDeathsPredictionByDep
+
+
+# METHODS
+def covidDeathsByCountry(data: DataFrame):
+
+    data_options = st.multiselect('Select fields: ', data.columns)
+
+    try:
+        df = data[data_options[0]]
+        country_options = st.multiselect('Select country', df)
+
+        country = data.loc[data[data_options[0]] == country_options[0]]
+
+        size = country.columns.__len__()
+
+        # Generate Graph
+        x = country.columns[4:size-1]
+        x = pd.to_datetime(x, format='%m/%d/%y')
+
+        deaths = country.loc[:, country.columns[4]:country.columns[size-1]]
+        data_index = deaths.index[0]
+
+        d = {
+            'columns': country.columns[4:size-1],
+            'data': [deaths.loc[data_index]],
+            'index': [1]
+        }
+
+        # plot graph
+        try:
+            st.set_option('deprecation.showPyplotGlobalUse', False)
+            df = pd.DataFrame(
+                d['data'], columns=d['columns'], index=d['index'])
+            df.columns.names = ['Covid deaths']
+            row = df.iloc[0]
+            row.plot(kind="line")
+            plt.show()
+            st.pyplot()
+
+        except:
+            st.warning('The graph could not be generated')
+
+    except:
+        st.warning("Please select a field")
+
+
+# Tendencia de la infección por Covid-19 en un País.
+def covidInfectionTendence(data: DataFrame):
+
+    data_options = st.multiselect('Select fields', data.columns)
+    # st.write(data_options)
+    try:
+        df = data[data_options[0]]
+        country_options = st.multiselect('Select country', df)
+        # st.write(country_options)
+        # st.write(df.head(2))
+
+        country = data.loc[data[data_options[0]] == country_options[0]]
+
+        # st.write(country.columns.__len__())
+        size = country.columns.__len__()
+
+        #st.write(country.columns[4:size - 1])
+        # st.write(country[country.columns[4]])
+
+        # Generate graph
+        x = country.columns[4:size-1]  # -> date
+        x = pd.to_datetime(x, format='%m/%d/%y')
+        # data.index = x
+        positive_cases = country.loc[:, country.columns[4]                                     :country.columns[size - 1]]  # -> positive_cases
+
+        data_index = positive_cases.index[0]
+        st.write(positive_cases.loc[data_index])
+
+        d = {'columns': country.columns[4:size - 1],
+             'data': [positive_cases.loc[data_index]], 'index': [1]}
+
+        # st.write(xd)
+
+        # Plot graph
+        try:
+
+            df = pd.DataFrame(
+                d['data'], columns=d['columns'], index=d['index'])
+            df.columns.names = ['COVID INFECTIONS']
+            row = df.iloc[0]
+            row.plot(kind="line")
+            plt.show()
+            st.pyplot()
+
+        except:
+            st.warning('The graph could not be generated')
+
+    except:
+        st.warning("Please select a field")
+
+
+
+# ===================== END METHODS =====================
 
 # Sidebar option tuple
 sid_opt_tuple = ('COVID Cases', 'COVID Deaths', 'Vaccines')
@@ -64,7 +172,6 @@ if upload_file is not None:
             covidDeathsByCountry(data)
         elif select_report == 'Predicción de mortalidad por COVID en un Departamento.':
             covidDeathsPredictionByDep(data)
-
 
     elif sidebar_selectbox == 'Vaccines':
         pass
